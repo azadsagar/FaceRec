e62ee9b3467dc3c6c4eca22cb41db056c16b7228
azadsagar <azadsagar@hotmail.com>
2015-01-31 22:58:14 +0530

    added DetectFace function and some images for database and testing

---
+++ DetectFace.m
@@ -0,0 +1,9 @@
+%function detects face in a given image and return box cordinate (2x2
+%matrix)
+
+function bbox=DetectFace(img)
+
+faceDetector=vision.CascadeObjectDetector();
+bbox=step(faceDetector,img);
+
+end
