4557081e9d689935ced16dd4f8253bdc369f4161
azadsagar <azadsagar@hotmail.com>
2015-02-12 01:01:57 +0530

    DetectFace now returns cropped face image

--- DetectFace.m
+++ DetectFace.m
@@ -1,9 +1,10 @@
 %function detects face in a given image and return box cordinate (2x2
 %matrix)
 
-function bbox=DetectFace(img)
+function faceImg=DetectFace(img)
 
 faceDetector=vision.CascadeObjectDetector();
 bbox=step(faceDetector,img);
+faceImg=imcrop(img,bbox);
 
 end
